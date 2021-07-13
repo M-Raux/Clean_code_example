451ec33de19977b44d9811bfbfa2d4a62951de0c
Maxence Raux <mraux@norsys.fr>
2021-07-13 10:31:42 +0200

    Functions

--- src/Parcel.java
+++ src/Parcel.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Parcel {
     private static final int MAX_SOIL_QUALITY = 1000;
     private static final int FERTILIZER_BONUS = 300;
@@ -8,6 +10,7 @@ public class Parcel {
     private static final int TYPE_B_HARVEST_GAIN = 2;
     private static final int PLANT_AGE_TO_BE_READY_TO_HARVEST = 10;
     private static final int PLANT_LIFESPAN = 11;
+    private static final String[] PLANT_TYPES = {"A", "B"};
 
     public static final int INVALID_PLANT_ERROR = -1;
     public static final int INVALID_ACTION_ERROR = -2;
@@ -25,45 +28,105 @@ public class Parcel {
     public int playNextTurn(String action) {
         int pointsEarned = 0;
         if (action != null) {
-            if (action.equals("FERTILIZE")) {
-                soilQuality = Math.min(MAX_SOIL_QUALITY, soilQuality + FERTILIZER_BONUS);
-            } else if (action.equals("HARVEST")) {
-                if (plantGrowth != NO_PLANT_VALUE) {
-                    if (plantGrowth >= PLANT_AGE_TO_BE_READY_TO_HARVEST && plantGrowth < PLANT_LIFESPAN) {
-                        if (plantType.equals("A")) {
-                            pointsEarned = TYPE_A_HARVEST_GAIN;
-                        } else if (plantType.equals("B")) {
-                            pointsEarned = TYPE_B_HARVEST_GAIN;
-                        }
-                    }
-                    plantGrowth = NO_PLANT_VALUE;
-                }
-            } else if (action.startsWith("PLANT")) {
-                if (plantGrowth == NO_PLANT_VALUE) {
-                    String chosenPlantType = action.substring(5);
-                    if (chosenPlantType.equals("A") || chosenPlantType.equals("B")) {
-                        plantType = chosenPlantType;
-                        plantGrowth = 0;
-                    } else {
-                        return INVALID_PLANT_ERROR;
-                    }
-                }
-            } else {
-                return INVALID_ACTION_ERROR;
-            }
+            pointsEarned = playAction(action);
         }
-        if (plantGrowth >= 0) {
-            plantGrowth++;
-            if (plantType.equals("A")) {
-                soilQuality = Math.max(0, soilQuality - TYPE_A_NUTRIENT_NEED);
-            } else if (plantType.equals("B")) {
-                soilQuality = Math.max(0, soilQuality - TYPE_B_NUTRIENT_NEED);
-            }
-            if (soilQuality == 0) {
-                plantGrowth = PLANT_LIFESPAN;
+        if (hasPlant()) {
+            growPlant();
+            alterSoilQualityFromPlant();
+            if (hasBadSoilQuality()) {
+                killPlant();
             }
         }
         return pointsEarned;
+    }
+
+    private boolean hasBadSoilQuality() {
+        return soilQuality == 0;
+    }
+
+    private int playAction(String action) {
+        int pointsEarned = 0;
+        if (action.equals("FERTILIZE")) {
+            pointsEarned = fertilize();
+        } else if (action.equals("HARVEST")) {
+            pointsEarned = harvest();
+        } else if (action.startsWith("PLANT")) {
+            String chosenPlantType = action.substring(5);
+            pointsEarned = plant(chosenPlantType);
+        } else {
+            return INVALID_ACTION_ERROR;
+        }
+        return pointsEarned;
+    }
+
+    private int fertilize() {
+        soilQuality = Math.min(MAX_SOIL_QUALITY, soilQuality + FERTILIZER_BONUS);
+        return 0;
+    }
+
+    private int harvest() {
+        int pointsEarned = 0;
+        if (hasPlant()) {
+            pointsEarned = getPlantValue();
+            removePlant();
+        }
+        return pointsEarned;
+    }
+
+    private int plant(String chosenPlantType) {
+        if (!hasPlant()) {
+            if (checkValidPlantType(chosenPlantType)) {
+                plantGrowth = 0;
+                plantType = chosenPlantType;
+            } else {
+                return INVALID_PLANT_ERROR;
+            }
+        }
+        return 0;
+    }
+
+    private boolean checkValidPlantType(String plantType) {
+        return Arrays.asList(PLANT_TYPES).contains(plantType);
+    }
+
+    private int getPlantValue() {
+        int plantValue = 0;
+        if (plantIsReady()) {
+            if (plantType.equals("A")) {
+                plantValue = TYPE_A_HARVEST_GAIN;
+            } else if (plantType.equals("B")) {
+                plantValue = TYPE_B_HARVEST_GAIN;
+            }
+        }
+        return plantValue;
+    }
+
+    private void alterSoilQualityFromPlant() {
+        if (plantType.equals("A")) {
+            soilQuality = Math.max(0, soilQuality - TYPE_A_NUTRIENT_NEED);
+        } else if (plantType.equals("B")) {
+            soilQuality = Math.max(0, soilQuality - TYPE_B_NUTRIENT_NEED);
+        }
+    }
+
+    private void growPlant() {
+        plantGrowth++;
+    }
+
+    private void killPlant() {
+        plantGrowth = PLANT_LIFESPAN;
+    }
+
+    private boolean plantIsReady() {
+        return plantGrowth >= PLANT_AGE_TO_BE_READY_TO_HARVEST && plantGrowth <= PLANT_LIFESPAN;
+    }
+
+    private boolean hasPlant() {
+        return plantGrowth != NO_PLANT_VALUE;
+    }
+
+    private void removePlant() {
+        plantGrowth = NO_PLANT_VALUE;
     }
 
     @Override
